81900f647890df2925a3f325a60c4f4ec0ae1e5d
Arunima Sahu <arunima.ata@gmail.com>
2025-11-19 02:05:20 +0530

    Update musiclib.py

---
+++ JARVIS/musiclib.py
@@ -0,0 +1,7 @@
+#music library
+music = {
+    "dandelions" : "https://www.youtube.com/watch?v=WgTMeICssXY&list=RDWgTMeICssXY&start_radio=1",  
+    "Woman":"https://www.youtube.com/watch?v=g7X9X6TlrUo&list=RDg7X9X6TlrUo&start_radio=1", 
+    "criminal": "https://www.youtube.com/watch?v=PvKw4ljBohA&list=RDPvKw4ljBohA&start_radio=1", 
+    "zara sa":"https://www.youtube.com/watch?v=_97T-Qqm1Xc&list=RD_97T-Qqm1Xc&start_radio=1"
+    }
